35b88645ff3309308bc196a2dd38861a50e2a21c
mattccs <mattcc.shek@gmail.com>
2022-05-03 12:54:24 +0100

    added eslint, started working on Art index

--- src/components/artworks/ArtIndex.js
+++ src/components/artworks/ArtIndex.js
@@ -1,6 +1,30 @@
-import react from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+// import bootstrap components
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+
 
 const ArtIndex = () => {
+  // breadlist state
+  const [ artList, setArtList] = useState([])
+
+  useEffect(() => {
+    const getArtList = async () => {
+      try {
+        const { data } = await axios.get('https://api.artic.edu/api/v1/artworks')
+        setArtList(data)
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+    getArtList()
+  }, [])
   return (
     <h1>Art Index / The Collection</h1>
   )
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css' 
+import './styles/main.scss'
+import App from './App'
+
+createRoot(document.getElementById('root')).render(<App />)
